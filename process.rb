4e76e48ab3549100dc7c6c3fcab07e8eb9473552
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-01-19 12:03:19 -0500

    Get fed up with crappy pandoc pdf bibliography and force through biblatex again.

--- process.rb
+++ process.rb
@@ -10,21 +10,29 @@ metadata = YAML.load_file('metadata.yml')
 files = metadata["file_order"].join(" ")
 
 # Add templating information for pdf
-pdf_opts = "--pdf-engine=xelatex --template=#{metadata["template"]} "
+tex_opts = "--biblatex --template=#{metadata["template"]} "
+# pdf_opts = "--biblatex --pdf-engine=xelatex --template=#{metadata["template"]} "
 html_opts = "--template=#{metadata["template-html"]} "
 
-pdf_cmd = "pandoc -sr markdown+yaml_metadata_block+citations \
-  #{pdf_opts} \
+tex_cmd = "pandoc -sr markdown+yaml_metadata_block+citations \
+  #{tex_opts} \
   --filter pandoc-citeproc 'metadata.yml' \
   #{files} \
-  -o docs/syllabus.pdf"
+  -o out.tex; xelatex out.tex; biber out; xelatex out.tex; xelatex out.tex; mv out.pdf docs/syllabus.pdf"
+# pdf_cmd = "pandoc -sr markdown+yaml_metadata_block+citations \
+#   #{pdf_opts} \
+#   --filter pandoc-citeproc 'metadata.yml' \
+#   #{files} \
+#   -o docs/syllabus.pdf"
 html_cmd = "pandoc -sr markdown+yaml_metadata_block+citations \
   #{html_opts} \
   --filter pandoc-citeproc 'metadata.yml' \
   #{files} \
   -o docs/index.html"
 
-system pdf_cmd
+system tex_cmd
+# puts "Generated out.tex"
+# system pdf_cmd
 puts "Generated docs/syllabus.pdf"
 system html_cmd
 puts "Generated docs/index.html"
